74493756796087acf70c1e7ca23f99ce97b7d514
Andrew Stelmach <ukiandre01@outlook.com>
2021-02-23 01:23:24 -0500

    Started GUI work and added functionality to get user input

---
+++ src/Ask.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Ask {
+  private JButton submitButton;
+  private JPanel panelMain;
+  private JTextField textField;
+  private List<String> userInput;
+
+
+  public Ask() {
+    textField.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        super.mouseEntered(e);
+
+        if (textField.getText().equals("Enter your tasks...")) textField.setText("");
+
+
+      }
+    });
+    submitButton.addActionListener(e ->
+            userInput = Arrays.asList(submitButton.getText().split(",")));
+  }
+
+  public JPanel contentPane() {
+    return this.panelMain;
+  }
+
+  public List<String> getUserInput() {
+    return userInput;
+  }
+}
--- src/Organizer.java
+++ src/Organizer.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+
 public class Organizer {
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Organizer");
+        frame.setContentPane(new Ask().contentPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    // TODO: ask user what they want to do in the day
+    // TODO: create list of tasks
+    // TODO: dynamically update list based on times entered
+    // TODO: send desktop notifications
+    // TODO: update list depending on what user clicked on notif
+    // TODO: add personal schedule of classes so the scheduler works around it
+
 }
